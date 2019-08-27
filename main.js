e458d9b03fe26acab840c8ceb9c4e442c633737e
nokazn <baleariclemon@gmail.com>
2019-08-27 23:54:56 +0900

    refactored

--- main.js
+++ main.js
@@ -1,258 +1,254 @@
 'user strict'
 
-const Store = (() => {
-  const wm = new WeakMap()
-  const _private = self => {
-    return wm.get(self) || wm.set(self, {}).get(self)
-  }
-  return class Store {
-    constructor () {
-      this.lastFormulaObj = {}
-      this.tmpFormulaObj = {
-        num1: '',
-        num2: '',
-        ope: ''
-      }
-      this.answer = '0'
-
-      _private(this).hasNum = num => {
-        return /^[+-]?\d+$/.test(num)
-      }
-    }
-
-    get num1 () {
-      return this.tmpFormulaObj.num1
-    }
-
-    set num1 (inputNum) {
-      if (inputNum === '0') {
-        return
-      } else if (inputNum === '.'){
-        inputNum = '0.'
-      } else if (inputNum == null) {
-        inputNum = ''
-      }
-      this.tmpFormulaObj.num1 = inputNum
-    }
-
-    get num2 () {
-      return this.tmpFormulaObj.num2
-    }
-
-    set num2 (inputNum) {
-      if (inputNum === '0') {
-        return
-      } else if (inputNum === '.'){
-        inputNum = '0.'
-      } else if (inputNum == null) {
-        inputNum = ''
-      }
-      this.tmpFormulaObj.num2 = inputNum
-    }
-
-    get ope () {
-      return this.tmpFormulaObj.ope
-    }
-
-    set setOpe (inputOpe) {
-      this.tmpFormulaObj.ope = inputOpe != null ?  inputOpe : ''
-    }
-
-    get hasNum1 () {
-      return _private(this).hasNum(this.tmpFormulaObj.num1)
-    }
-
-    get hasNum2 () {
-      return _private(this).hasNum(this.tmpFormulaObj.num2)
-    }
-
-    get hasOpe () {
-      return !!this.tmpFormulaObj.ope
-    }
-
-    get isFirstFormula () {
-      return !this.lastFormulaObj.ope
-    }
-
-    get tmpFormula () {
-      const formula = store.tmpFormulaObj
-      return `${formula.num1}${formula.ope}${formula.num2}`
-    }
-
-    get isNegativeNum1 () {
-      return store.num1.indexOf('-') !== -1
-    }
-
-    get isNegativeNum2 () {
-      return store.num2.indexOf('-') !== -1
-    }
-
-    initTmpFormula (num1, num2, ope) {
-      this.lastFormulaObj = {...this.tmpFormulaObj}
-      this.tmpFormulaObj = {
-        num1: num1 || '',
-        num2: num2 || '',
-        ope: ope || ''
-      }
-    }
-
-    clearTmpFormula () {
-      this.tmpFormulaObj = {
-        num1: '',
-        num2: '',
-        ope: ''
-      }
-      this.answer = 0
-    }
-
-    calc () {
-      this.answer = (new Function (`"use strict"; return ${this.tmpFormula}`))().toString()
-    }
-
-    negate () {
-      if (this.hasOpe) {
-        console.log(this.isNegativeNum1)
-        this.num2 = this.isNegativeNum2 ? this.num1.substr(2).slice(0, -1) : `(-${this.num2})`
-      } else {
-        console.log(this.isNegativeNum1)
-        this.num1 = this.isNegativeNum1 ? this.num1.substr(2).slice(0, -1) : `(-${this.num1})`
-      }
-    }
-  }
-})()
-
-Viewer = {
-  _replaceFormula (formula) {
-    return formula.replace(/\*|\//g, (match) => {
-      return match === '*' ? '×' : match === '/' ? '÷' : match
-    })
-  },
-
-  updateTmpFormulaBox (inputVal) {
-    inputVal = this._replaceFormula(inputVal)
-    document.getElementById('tmpFormulaBox').innerText = inputVal
-  },
-
-  pushTmpFormulaBox (inputVal) {
-    inputVal = this._replaceFormula(inputVal)
-    document.getElementById('tmpFormulaBox').innerText += inputVal
-  },
-
-  replaceTmpFormulaBox (inputVal, count = 1) {
-    const text = document.getElementById('tmpFormulaBox').innerText
-    const formula = this._replaceFormula(`${text.slice(0, -count)}${inputVal}`)
-    document.getElementById('tmpFormulaBox').innerText = formula
-  },
-
-  updateAnswerBox (answer) {
-    // answer =  answer.replace(/\((.*)\)/, '$1')
-    document.getElementById('answerBox').innerText = answer || '0'
-  },
-}
-
-const store = new Store()
-Viewer.updateAnswerBox('0')
-
-const buttonList = document.getElementsByTagName('button')
-Array.prototype.forEach.call(buttonList, (ele) => {
-  ele.addEventListener('click', (e) => {
-    const inputVal = e.target.innerText
-    console.log(store)
-    console.log(store.tmpFormulaObj)
-    console.log(store.tmpFormula)
-  })
-})
-
 document.addEventListener('keydown', e => {
-  console.log(e.key)
   if (/^[0-9]$|\./.test(e.key)) num(e.key)
   else if (/^\+|-|\*|\/$/.test(e.key)) ope(e.key)
   else if (e.key === 'Escape') clearAll()
   else if (e.key === 'Delete') cancel()
   else if (e.key === 'Backspace') backSpace()
   else if (e.key === '=') equ()
+  else return
+  setTimeout(() => {console.log(viewer)}, 0)
+  setTimeout(() => {console.log(store.nums._q)}, 0)
+  setTimeout(() => {console.log(store.opes._q)}, 0)
 })
 
-function ope (inputOpe) {
-  if (store.hasOpe) {
-    if (store.hasNum2) {
-      store.calc()
-      Viewer.pushTmpFormulaBox(`${store.num2}${inputOpe}`)
-      Viewer.updateAnswerBox(store.answer)
-      store.initTmpFormula(store.answer, null, inputOpe)
+class Queue {
+  constructor (length) {
+    if (typeof length !== 'number') {
+      throw new SyntaxError('Invalid or unexpected token')
+    } else if (length <= 0) {
+      throw new RangeError('Invalid queue length')
+    }
+    this._q = new Array(length || 0).fill('')
+    Object.defineProperty(this, 'length', {
+      value: length,
+      writable: false
+    })
+  }
+
+  get (index) {
+    if (typeof index !== 'number') {
+      throw new SyntaxError('Invalid or unexpected')
+    } else if (index >= this.length || index < 0) {
+      throw new RangeError('Invalid index')
+    }
+    return this._q[index]
+  }
+
+  // get indexOfLastVal () {
+  //   const index = this._q.indexOf('')
+  //   return (index !== -1 ? index : this.length) - 1
+  // }
+
+  // get lastVal () {
+  //   const index = this.indexOfLastVal
+  //   return this._q[index] || null
+  // }
+
+  // get lastIndexOfEmpty () {
+  //   return this._q.lastIndexOf('')
+  // }
+
+  // get indexOfEmpty () {
+  //   return this._q.indexOf('')
+  // }
+  // get enterableIndex () {
+  //   const reversedIndex = this._q.slice().reverse().findIndex(ele => ele !== '')
+  //   if (reversedIndex === -1) {
+  //     return 0
+  //   } else if (!reversedIndex){
+  //     return -1
+  //   } else {
+  //     return this.length - reversedIndex
+  //   }
+  // }
+
+  enqueue (val) {
+    if (this._q.slice(-1)[0] === '') {
+      this._q[this.length - 1] = val
+      // this._q.splice(-1, 1, val)
     } else {
-      store.setOpe = inputOpe
-      Viewer.replaceTmpFormulaBox(inputOpe)
+      this._q.splice(0, 1)
+      this._q.push(val)
     }
-  } else {
-    console.log(store.hasNum1)
-    if (!store.hasNum1) {
-      store.num1 = store.answer
+  }
+
+  dequeue (count = 1) {
+    if (typeof count !== 'number') {
+      throw new SyntaxError('Invalid or unexpected token')
+    } else if (count < 1) {
+      throw new RangeError('Invalid dequeue counts')
     }
-    store.setOpe = inputOpe
-    Viewer.updateTmpFormulaBox(store.tmpFormula)
+    this._q.splice(0, count)
+    this._q = count === 1 ? [...this._q, ''] : this._q.concat(new Array(count).fill(''))
   }
 }
 
-function equ () {
-  if (store.hasNum1) {
-    if (store.hasOpe) {
-      if (!store.hasNum2) {
-        store.num2 = store.num1
-      }
-    } else {
-      store.setOpe = store.lastFormulaObj.ope
-      store.num2 = store.lastFormulaObj.num2
-    }
-  } else {
-    store.num1 = store.answer
-    store.setOpe = store.lastFormulaObj.ope
-    store.num2 = store.lastFormulaObj.num2
+class Store {
+  constructor () {
+    this.nums = new Queue(2)
+    this.opes = new Queue(2)
   }
-  store.calc()
-  Viewer.updateAnswerBox(store.answer)
-  Viewer.updateTmpFormulaBox('')
-  store.initTmpFormula()
+
+  get num () {
+    return this.nums.get(1)
+  }
+
+  set num (num) {
+    this.nums.enqueue(num)
+  }
+
+  get ope () {
+    return this.opes.get(1)
+  }
+
+  set ope(ope) {
+    this.opes.enqueue(ope)
+  }
+
+  get formula () {
+    return `${this.nums.get(0)}${this.ope}${this.nums.get(1)}`
+  }
+
+  get isCalculatable () {
+    return this.nums._q.every(ele => !!ele) && this.ope
+  }
+
+  init () {
+    this.nums.dequeue(this.length)
+    this.opes.dequeue(this.length)
+  }
+
+  calc () {
+    if (this.isCalculatable) {
+      const answer = (new Function(`"use strict"; return ${this.formula}`))().toString()
+      this.nums.enqueue(answer)
+      this.opes.dequeue()
+    } else {
+      console.error("this formula is uncalclatable!")
+    }
+  }
+}
+
+class Viewer {
+  constructor () {
+    this._num = ''
+    this._ope = ''
+  }
+
+  get num () {
+    return this._num
+  }
+
+  set num (num) {
+    if (!this._num) {
+      if (num === '0') {
+        this._num = ''
+      } else if (num === '.') {
+        this._num = '0.'
+      } else {
+        this._num = num
+      }
+    } else if (!this._num.includes('.') || num.slice(-1) !== '.') {
+      this._num = num
+    }
+  }
+
+  get ope () {
+    return this._ope
+  }
+
+  set ope (ope) {
+    this._ope = ope
+  }
+
+  get tmpFormulaBox () {
+    return document.getElementById('tmpFormulaBox').innerText
+  }
+
+  set tmpFormulaBox (val) {
+    document.getElementById('tmpFormulaBox').innerText= val
+  }
+
+  get answerBox () {
+    return document.getElementById('answerBox').innerText
+  }
+
+  set answerBox (val) {
+    document.getElementById('answerBox').innerText = val || '0'
+  }
 }
 
 function num (inputNum) {
-  if (store.hasOpe) {
-    store.num2 += inputNum
-    Viewer.updateAnswerBox(store.num2)
+  // 確定した ope を store に送る
+  if (viewer.ope) {
+    store.ope = viewer.ope
+    viewer.ope = ''
+  }
+  viewer.num += inputNum
+  viewer.answerBox = viewer.num
+}
+
+function ope (inputOpe) {
+  // ope が仮確定の場合か未確定の場合か
+  if (viewer.ope) {
+    viewer.tmpFormulaBox = viewer.tmpFormulaBox.slice(0, -1)
+    viewer.tmpFormulaBox += inputOpe
   } else {
-    store.num1 += inputNum
-    Viewer.updateAnswerBox(store.num1)
+    viewer.tmpFormulaBox += (viewer.num || viewer.answerBox) + inputOpe
+  }
+  // num を store に送る
+  store.num = viewer.num || viewer.answerBox
+  viewer.num = ''
+
+  if (store.isCalculatable) {
+    store.calc()
+    viewer.answerBox = store.num
+  }
+  viewer.ope = inputOpe
+}
+
+function equ () {
+  // TODO
+  if (store.num) {
+    if (!store.ope) {
+      store.ope = viewer.ope || store.opes.get(0)
+      viewer.ope = ''
+    }
+    // num を store に送る
+    store.num = viewer.num || store.nums.get(0) || viewer.answerBox
+    viewer.num = ''
+    store.calc()
+    viewer.answerBox = store.num
+    viewer.tmpFormulaBox = ''
   }
 }
 
 function clearAll () {
-  store.clearTmpFormula()
-  Viewer.updateAnswerBox('0')
-  Viewer.updateTmpFormulaBox('')
+  store.init()
+  viewer.num = ''
+  viewer.ope = ''
+  viewer.tmpFormulaBox = ''
+  viewer.answerBox = ''
 }
 
 function cancel () {
-  if (store.hasOpe) {
-    store.setNum2 = ''
-    Viewer.updateAnswerBox('0')
-  } else {
-    store.num1 = ''
-    Viewer.updateAnswerBox('0')
-  }
+  viewer.num = ''
+  viewer.answerBox = viewer.num
 }
 
 function backSpace () {
-  if (store.hasOpe) {
-    store.num2 = store.num2.slice(0, -1)
-    Viewer.updateAnswerBox(store.num2)
-  } else {
-    store.num1 = store.num1.slice(0, -1)
-    Viewer.updateAnswerBox(store.num1)
-  }
+  viewer.num = viewer.num.slice(0, -1)
+  viewer.answerBox = viewer.num
 }
 
 function negate () {
-  return
-  store.negate()
-  Viewer.updateAnswerBox(store.hasOpe ? store.num2 : store.num1)
+  viewer.num = (Number(viewer.num) * -1).toString()
+  viewer.answerBox = viewer.num
 }
+
+const store = new Store()
+const viewer = new Viewer()
+viewer.answerBox = ''
+viewer.tmpFormulaBox = ''
